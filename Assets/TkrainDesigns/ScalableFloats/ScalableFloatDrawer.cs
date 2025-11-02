60f394c3e3adceed088b7ea450a836932b01719b
Brian K. Trotter <mnemoth42@gmail.com>
2025-11-01 23:46:48 -0700

    Introduce scalable float system with serialized properties and editor support

---
+++ Assets/TkrainDesigns/ScalableFloats/ScalableFloatDrawer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TkrainDesigns.ScalableFloats
+{
+    [CustomPropertyDrawer(typeof(ScalableFloat))]
+    public class ScalableFloatDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            
+            // Draw label
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            
+            // Don't indent child fields
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            
+            // Calculate rects
+            float valueWidth = position.width * 0.3f;
+            float curveWidth = position.width * 0.7f - 5f;
+            
+            Rect valueRect = new Rect(position.x, position.y, valueWidth, position.height);
+            Rect curveRect = new Rect(position.x + valueWidth + 5f, position.y, curveWidth, position.height);
+            
+            // Get properties
+            SerializedProperty valueProp = property.FindPropertyRelative("<Value>k__BackingField");
+            SerializedProperty curveProp = property.FindPropertyRelative("<Curve>k__BackingField");
+            
+            // Draw fields
+            EditorGUI.PropertyField(valueRect, valueProp, GUIContent.none);
+            EditorGUI.PropertyField(curveRect, curveProp, GUIContent.none);
+            
+            // Restore indent
+            EditorGUI.indentLevel = indent;
+            
+            EditorGUI.EndProperty();
+        }
+        
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
---
+++ Assets/TkrainDesigns/ScalableFloats/Scripts/ScalableFloat.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace TkrainDesigns.ScalableFloats
+{
+    [System.Serializable]
+    public struct ScalableFloat
+    {
+        [field: SerializeField] public float Value { get; private set; } 
+        [field: SerializeField] public AnimationCurve Curve { get; private set; }
+        
+        
+        public ScalableFloat(float value, AnimationCurve curve)
+        {
+            Value = value;
+            Curve = curve;
+        }
+        
+        public float Evaluate(float level) => Curve.Evaluate(level) * Value;
+        
+        public int EvaluateInt(float level) => Mathf.RoundToInt(Evaluate(level));
+        
+        public bool EvaluateBool(float level) => Evaluate(level) > 0;
+        
+    }
+}
