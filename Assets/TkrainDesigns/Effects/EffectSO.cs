f1c54948e481d08eea55c7c9ee8a5d839729a2cb
Brian K. Trotter <mnemoth42@gmail.com>
2026-02-01 21:08:51 -0800

    Add Effects system with `EffectSO` ScriptableObject and assembly definition
    - Introduced `EffectSO` with properties for name, description, duration type, and modifiers.
    - Created `TkrainDesigns-Effects` assembly definition and supporting files.

---
+++ Assets/TkrainDesigns/Effects/EffectSO.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using TkrainDesigns.Attributes;
+using TkrainDesigns.ScalableFloats;
+using UnityEngine;
+
+namespace TkrainDesigns.Effects
+{
+    public enum DurationType
+    {
+        Instant,
+        Duration,
+        Infinite,
+    }
+    
+    [CreateAssetMenu(fileName = "Effect", menuName = "TkrainDesigns/Effects", order = 0)]
+    public class EffectSO : ScriptableObject
+    {
+        [field: SerializeField] public string Name { get; private set; }= "";
+        [field: SerializeField] public string Description { get; private set; }= "";
+        [field: SerializeField] public DurationType DurationType { get; private set; } = DurationType.Instant;
+        [field: SerializeField] public ScalableFloat Duration { get; private set; }
+        [field: SerializeField] public ScalableFloat Period { get; private set; }
+        [field: SerializeField] public List<AttributeModifier> AttributeModifier { get; private set; }
+        
+    }
+}
